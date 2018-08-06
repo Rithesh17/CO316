f89c76120bf780c20d35fc0ba54edc32f0d0965f
Rithesh17 <rithhem20@gmail.com>
2018-08-06 08:38:06 +0530

    Image blur

--- Assignments/A2/ImgBlur/template.cu
+++ Assignments/A2/ImgBlur/template.cu
@@ -11,33 +11,37 @@ __global__ void imgBlur(float* imgIn, float* imgOut, int imageWidth, int imageHe
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   int idy = blockIdx.y * blockDim.y + threadIdx.y;
 
-  __shared__ int count[25];
-  __shared__ float sum[25];
+  if(idx<imageWidth && idy<imageHeight)
+ {
+  float sum = imgIn[idx*imageWidth+idy];
+  
+  if(idx>0 && idy>0)
+    sum += imgIn[(idx-1)*imageWidth+(idy-1)];
 
-  int i;
+  if(idx>0)
+    sum += imgIn[(idx-1)*imageWidth+idy];
 
-  for(i=0; i<25; i++)
-  {
-    count[i] = 0;
-    sum[i] = 0;
-  }
+  if(idx<imageWidth-1)
+    sum += imgIn[(idx+1)*imageWidth+idy];
 
-  if(idx < imageWidth && idy < imageHeight)
-  {
-    int blurWindowNo = (idx/BLUR_SIZE)*imageWidth+(idy/BLUR_SIZE);
+  if(idx<imageWidth-1 && idy<imageHeight-1)
+    sum += imgIn[(idx+1)*imageWidth+idy+1];
 
-    __syncthreads();
+  if(idx<imageWidth && idy>0)
+    sum += imgIn[(idx+1)*imageWidth+idy-1];
 
-    count[blurWindowNo] ++;
+  if(idy>0)
+    sum += imgIn[idx*imageWidth+idy-1];
 
-    __syncthreads();
+  if(idy<imageHeight)
+    sum += imgIn[idx*imageWidth+idy+1];
 
-    sum[blurWindowNo] += imgIn[idx*imageWidth+idy];
+  if(idx>0 && idy<imageHeight)
+    sum += imgIn[(idx-1)*imageWidth+idy+1];
 
-    __syncthreads();
+  imgOut[idx*imageWidth+idy] = sum / (float)(BLUR_SIZE*BLUR_SIZE);
 
-    imgOut[idx*imageWidth+idy] = (float)sum[blurWindowNo] / count[blurWindowNo];
-  }
+ }   
 }
 
 int main(int argc, char *argv[]) {
