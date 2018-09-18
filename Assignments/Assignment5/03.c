ddfc141689cd1ccb5c8b7e34619683f98c4a7691
Rithesh17 <rithhem20@gmail.com>
2018-09-18 21:03:40 +0530

    Added 01.c

---
+++ Assignments/Assignment5/03.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+#include<omp.h>
+
+#define MAX_THREADS 56
+#define MAX(a, b) (a>b)?a:b
+
+unsigned long int i, n;
+
+void daxpy_uniprocess(double* X, double* Y, int a)
+{
+  for(i=0; i<n; i++)
+    X[i] = a*X[i]+Y[i];
+}
+
+void daxpy_multi_process(double* X, double* Y, int a, unsigned long int num_threads)
+{
+  omp_set_num_threads(MAX(num_threads, MAX_THREADS));
+  int thread_id;
+  unsigned long int index;
+
+  #pragma omp parallel
+  {
+    thread_id = omp_get_thread_num();
+    for(i=0; i<n/num_threads; i++)
+    {
+      index = i*num_threads+thread_id;
+      X[index] = a*X[index]+Y[index];
+    }
+  }
+}
+
+int main()
+{
+  n = rand()%(1<<16);
+  double X[n], Y[n];
+
+  for(i=0; i<n; i++)
+  {
+    X[i] = rand() + rand() / RAND_MAX;
+    Y[i] = rand() + rand() / RAND_MAX;
+  }
+
+  int a = rand();
+
+  int start, end;
+
+  printf("Time taken by uniprocessor: ");
+  start = clock();
+  daxpy_uniprocess(X, Y, a);
+  end = clock();
+  printf("%fs\n\n", (double)(end - start)/CLOCKS_PER_SEC);
+
+  for(i=2; i<=80; i++)
+  {
+    printf("Time taken by %d threads: ", i);
+    start = clock();
+    daxpy_uniprocess(X, Y, a);
+    end = clock();
+    printf("%fs\n", (double)(end - start)/CLOCKS_PER_SEC);
+  }
+  printf("\n");
+}
